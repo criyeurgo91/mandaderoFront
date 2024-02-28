1595b43641213f501c40394fffc62e53c91148bc
Lenin Montilla <leninmontilla33@gmail.com>
2024-02-27 21:08:45 -0500

    Merge branch 'main' of https://github.com/criyeurgo91/mandaderoFront

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -1,15 +1,10 @@
-<<<<<<< HEAD
 
 //import Register from "../Register/Register"
 //import CreateUser from "../User/CreateUser"
 //import UserView from "../User/UserView"
 import ViewVehicle from '../vehicle/ViewVehicle'
 import CreateVehicle from '../vehicle/CreateVehicle'
-=======
 import React from "react"
-import CreateUser from "../User/CreateUser"
-import UserView from "../User/UserView"
->>>>>>> 774713adbca245279eac30c3896aed3779a5aff4
 
 
 const App = () => {
