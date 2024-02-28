9603125093a36bde2d03c673f513a5f9f49c61bf
cristianUrbano <criurbano316@hotmail.com>
2024-02-27 23:59:30 -0500

    cambios aprobados1

--- src/Pages/App/App.jsx
+++ src/Pages/App/App.jsx
@@ -1,24 +1,18 @@
+import React from "react";
+import Createuser from '../User/CreateUser'
 
-//import Register from "../Register/Register"
-//import CreateUser from "../User/CreateUser"
-//import UserView from "../User/UserView"
-import ViewVehicle from '../vehicle/ViewVehicle'
-import CreateVehicle from '../vehicle/CreateVehicle'
-import React from "react"
-<<<<<<< HEAD
-import CreateUser from "../User/CreateUser"
-import UserView from "../User/UserView"
-=======
->>>>>>> 1595b43641213f501c40394fffc62e53c91148bc
 
 
 const App = () => {
   return (
     <div>
-      <UserView/>
+      <Createuser/>
     </div>
   )
 }
 
 export default App
+
+
+
 
