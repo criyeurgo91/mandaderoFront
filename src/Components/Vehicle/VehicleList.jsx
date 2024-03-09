86fcf9f1e4857790cf595d44d0790581130416cd
junigomez <amaya261094gomez@gmail.com>
2024-03-08 21:15:30 -0500

    cambios de sidebar y vehicle list

--- src/Components/Vehicle/VehicleList.jsx
+++ src/Components/Vehicle/VehicleList.jsx
@@ -1,10 +1,8 @@
 import  { useState, useEffect } from 'react';
 import axios from 'axios';
-<<<<<<< HEAD
 import { useNavigate } from 'react-router-dom';
-=======
 import { Link } from 'react-router-dom';
->>>>>>> b0cf288532144c9da8a04375b786c7e6f0c5e9fe
+
 
 function VehicleList() {
   const [vehicles, setVehicles] = useState([]);
