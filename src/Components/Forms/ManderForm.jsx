a14c865a33abc010e6053418d4350e2c4e116d3c
quimbaya <john.quimbaya1@misena.edu.co>
2024-03-09 01:49:15 -0500

    cargue todos los datos

--- src/Components/Forms/ManderForm.jsx
+++ src/Components/Forms/ManderForm.jsx
@@ -1,25 +1,24 @@
 import { useState } from 'react';
 import axios from 'axios';
 
-const ManderForm = () => {
+function ManderForm() {
   const [formData, setFormData] = useState({
     email_account: '',
     password_account: '',
     name_user: '',
     lastname_user: '',
     phone_user: '',
-    ismander_user:false,
+    ismander_user: false,
     image_mander: null,
     address_mander: '',
     cc_mander: '',
-    ishavecar_mander:false,
-    ishavemoto_mander:false,
+    ishavecar_mander: false,
+    ishavemoto_mander: false,
     isactive_mander: false,
     isvalidate_mander: false,
-    
   });
 
-  const [message, setMessage]=useState('')
+  const [message, setMessage] = useState('');
 
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
@@ -58,13 +57,13 @@ const ManderForm = () => {
 
       const manderResponse = await axios.post('http://127.0.0.1:8000/api/mander/', {
         user_id_user: userId,
-      image_mander: formData.image_mander,
-      ishavecar_mander: formData.ishavecar_mander ? formData.ishavecar_mander : false, // Check si está marcado
-      ishavemoto_mander: formData.ishavemoto_mander ? formData.ishavemoto_mander : false, // Check si está marcado
-      address_mander: formData.address_mander,
-      cc_mander: formData.cc_mander,
-      isactive_mander: formData.isactive_mander ? formData.isactive_mander : false, // Check si está marcado
-      isvalidate_mander: formData.isvalidate_mander ? formData.isvalidate_mander : false, // Check si está marcado
+        image_mander: formData.image_mander,
+        ishavecar_mander: formData.ishavecar_mander ? formData.ishavecar_mander : false, // Check si está marcado
+        ishavemoto_mander: formData.ishavemoto_mander ? formData.ishavemoto_mander : false, // Check si está marcado
+        address_mander: formData.address_mander,
+        cc_mander: formData.cc_mander,
+        isactive_mander: formData.isactive_mander ? formData.isactive_mander : false, // Check si está marcado
+        isvalidate_mander: formData.isvalidate_mander ? formData.isvalidate_mander : false, // Check si está marcado
       });
       const manderId = manderResponse.data.id_mander;
 
@@ -72,24 +71,22 @@ const ManderForm = () => {
       // Manejo de respuesta exitosa
 
       // Limpiar Formulario
-    setFormData({
-      email_account: '',
-      password_account: '',
-      name_user: '',
-      lastname_user: '',
-      phone_user: '',
-      ismander_user: false,
-      image_mander: null,
-      address_mander: '',
-      cc_mander: '',
-      ishavecar_mander: false,
-      ishavemoto_mander: false,
-      isactive_mander: false,
-      isvalidate_mander: false,
-      message: 'Mander created successfully',
-    });
-    
-
+      setFormData({
+        email_account: '',
+        password_account: '',
+        name_user: '',
+        lastname_user: '',
+        phone_user: '',
+        ismander_user: false,
+        image_mander: null,
+        address_mander: '',
+        cc_mander: '',
+        ishavecar_mander: false,
+        ishavemoto_mander: false,
+        isactive_mander: false,
+        isvalidate_mander: false,
+      });
+      setMessage('Mander created successfully');
     } catch (error) {
       console.error('Error creating Mander, User, Account:', error);
       // Manejo de error
@@ -137,33 +134,27 @@ const ManderForm = () => {
         </div>
 
         {/* Campos para el mandadero */}
-
         <div>
           <label htmlFor="cc_mander" className="block">Cédula:</label>
           <input type="text" id="cc_mander" name="cc_mander" value={formData.cc_mander} onChange={handleChange} className="border border-gray-300 rounded px-3 py-2 w-full" />
         </div>
-
         <div>
           <label htmlFor="address_mander" className="block">Dirección:</label>
           <input type="text" id="address_mander" name="address_mander" value={formData.address_mander} onChange={handleChange} className="border border-gray-300 rounded px-3 py-2 w-full" />
         </div>
 
-        
         <div>
           <label htmlFor="ishavemoto_mander" className="form-checkbox">Is have a Bike?:</label>
           <input type="checkbox" name='ishavemoto_mander' id='ishavemoto_mander' onChange={handleChange} className="border border-gray-300 rounded px-3 py-2 w-full" />
         </div>
-
         <div>
           <label htmlFor="ishavecar_mander" className="form-checkbox">Is have a Car?:</label>
           <input type="checkbox" name='ishavecar_mander' id='ishavecar_mander' onChange={handleChange} className="border border-gray-300 rounded px-3 py-2 w-full" />
         </div>
-
         <div>
           <label htmlFor="isactive_mander" className="form-checkbox">Is Active?:</label>
           <input type="checkbox" name='isactive_mander' id='isactive_mander' onChange={handleChange} className="border border-gray-300 rounded px-3 py-2 w-full" />
         </div>
-
         <div>
           <label htmlFor="isvalidate_mander" className="form-checkbox">Is Validate?:</label>
           <input type="checkbox" name='isvalidate_mander' id='isvalidate_mander' onChange={handleChange} className="border border-gray-300 rounded px-3 py-2 w-full" />
@@ -174,6 +165,6 @@ const ManderForm = () => {
       {message && <p className="text-green-500">{message}</p>}
     </div>
   );
-};
+}
 
 export default ManderForm;
