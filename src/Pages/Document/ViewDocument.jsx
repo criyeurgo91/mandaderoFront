16f5c4fc3455444693de22c139c46350e6184f09
junigomez <amaya261094gomez@gmail.com>
2024-03-12 20:44:51 -0500

    se modifico el estilo de view list

--- src/Pages/Document/ViewDocument.jsx
+++ src/Pages/Document/ViewDocument.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 const ViewDocument = () => {
   const [documents, setDocuments] = useState([]);
-  const navigate = useNavigate(); // Obtener la función de navegación
+  const navigate = useNavigate(); 
 
   useEffect(() => {
     fetchData();
@@ -20,11 +20,11 @@ const ViewDocument = () => {
   };
 
   const handleVerifyDocument = () => {
-    navigate('/document/createdocument'); // Redirigir al formulario de documentos cuando se hace clic en el botón
+    navigate('/document/createdocument'); 
   };
 
   return (
-    <div className="max-w-4xl mx-auto p-6 bg-white rounded-lg shadow-md">
+    <div className="max-w-4xl mx-auto p-8 bg-white rounded-lg shadow-md">
       <h2 className="text-lg font-semibold mb-4">View Documents</h2>
       <button
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
@@ -32,30 +32,32 @@ const ViewDocument = () => {
       >
         Verify Document
       </button>
-      <table className="w-full border-collapse border border-gray-300">
-        <thead>
-          <tr className="bg-gray-200">
-            <th className="border border-gray-300 px-4 py-2">Document Image</th>
-            <th className="border border-gray-300 px-4 py-2">Is Document Vehicle</th>
-            <th className="border border-gray-300 px-4 py-2">Is Verified Document</th>
-            <th className="border border-gray-300 px-4 py-2">Document Type</th>
-            <th className="border border-gray-300 px-4 py-2">Date Verified Document</th>
-            <th className="border border-gray-300 px-4 py-2">User ID</th>
-          </tr>
-        </thead>
-        <tbody>
-          {documents.map((document) => (
-            <tr key={document.id} className="border border-gray-300">
-              <td className="border border-gray-300 px-4 py-2">{document.image_document}</td>
-              <td className="border border-gray-300 px-4 py-2">{document.isdocument_vehicle ? 'Yes' : 'No'}</td>
-              <td className="border border-gray-300 px-4 py-2">{document.isverified_document ? 'Yes' : 'No'}</td>
-              <td className="border border-gray-300 px-4 py-2">{document.type_document}</td>
-              <td className="border border-gray-300 px-4 py-2">{document.dateverified_document}</td>
-              <td className="border border-gray-300 px-4 py-2">{document.user_id_user}</td>
+      <div className="overflow-x-auto">
+        <table className="w-full border-collapse border border-gray-300">
+          <thead>
+            <tr className="bg-gray-200">
+              <th className="border border-gray-300 px-4 py-2">Document Image</th>
+              <th className="border border-gray-300 px-4 py-2">Is Document Vehicle</th>
+              <th className="border border-gray-300 px-4 py-2">Is Verified Document</th>
+              <th className="border border-gray-300 px-4 py-2">Document Type</th>
+              <th className="border border-gray-300 px-4 py-2">Date Verified Document</th>
+              <th className="border border-gray-300 px-4 py-2">User ID</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {documents.map((document) => (
+              <tr key={document.id} className="border border-gray-300">
+                <td className="border border-gray-300 px-4 py-2">{document.image_document}</td>
+                <td className="border border-gray-300 px-4 py-2">{document.isdocument_vehicle ? 'Yes' : 'No'}</td>
+                <td className="border border-gray-300 px-4 py-2">{document.isverified_document ? 'Yes' : 'No'}</td>
+                <td className="border border-gray-300 px-4 py-2">{document.type_document}</td>
+                <td className="border border-gray-300 px-4 py-2">{document.dateverified_document}</td>
+                <td className="border border-gray-300 px-4 py-2">{document.user_id_user}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
