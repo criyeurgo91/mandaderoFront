e59a793b39df157aed2f6e7a0cbd015a0d32bbc0
quimbaya <john.quimbaya1@misena.edu.co>
2024-03-22 18:15:04 -0500

    cargo actualizacion

--- src/Components/Forms/VehicleForm.jsx
+++ src/Components/Forms/VehicleForm.jsx
@@ -1,4 +1,4 @@
-import  { useState } from 'react';
+import { useState } from 'react';
 import axios from 'axios';
 
 const userdefault = 1; // se debe enviar un id de usuario por defecto.
@@ -14,9 +14,6 @@ const VehicleForm = () => {
     isverified_vehicle: false,
     user_id_user: userdefault,
   };
-  
-    
-
 
   const [formData, setFormData] = useState(initialState);
   const [loading, setLoading] = useState(false);
@@ -48,12 +45,13 @@ const VehicleForm = () => {
       image_vehicle: file
     });
   }
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     if(plateError){
       alert("Error Placa");
     }
-    console.log(formData);
+
     if (plateError || !formData.brand_vehicle || !formData.plate_vehicle || !formData.model_vehicle || !formData.color_vehicle || !formData.type_vehicle) {
       alert('Por favor, complete todos los campos correctamente antes de enviar.');
       return;
@@ -68,7 +66,6 @@ const VehicleForm = () => {
     formDataToSend.append('isverified_vehicle', formData.isverified_vehicle);
     formDataToSend.append('user_id_user', formData.user_id_user);
     
-    console.log(formDataToSend);
     try {
       setLoading(true);
       const response = await axios.post('https://manders.azurewebsites.net/api/vehicle/', formDataToSend);
@@ -77,16 +74,43 @@ const VehicleForm = () => {
       setLoading(false);
       alert('Formulario enviado exitosamente');
       window.location.href = '/Admin/vehicles';
-
     } catch (error) {
       console.error('Error:', error);
       setLoading(false);
       alert('Hubo un error al enviar el formulario');
     }
   };
-
   // Generar un array con las marcas de vehículos
-  const vehicleBrands = ['Chevrolet', 'Hyundai', 'Kia', 'Volkswagen', 'Ford', 'Nissan', 'Chana', 'Honda', 'Yamaha', 'Harley-Davidson', 'Kawasaki', 'Suzuki'];
+const vehicleBrandsCar = [
+  'Chevrolet', 'Hyundai', 'Kia', 'Volkswagen', 'Ford', 'Nissan', 'Chery', 'Honda', 'Toyota',
+  'Mitsubishi', 'Mazda','Fiat','Peugeot','Renauolt'
+];
+
+// Generar un array con las marcas de motos
+const vehicleBrandsBike = [
+  'Yamaha',  'Kawasaki', 'Suzuki',  
+  'Honda', 'KTM',  'Suzuki ', 
+  'Bajaj', 'Hero','Auteco','Akt','Victory','Tvs','Kymco','Pulsar',
+];
+
+// Variable para almacenar las marcas de vehículos a mostrar en función del tipo seleccionado
+const vehicleBrandsToShow = formData.type_vehicle === 'car' ? vehicleBrandsCar : vehicleBrandsBike;
+
+
+<select
+  name="brand_vehicle"
+  value={formData.brand_vehicle}
+  onChange={handleInputChange}
+  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+>
+  <option value="">Seleciona la Marca</option>
+  {vehicleBrandsToShow.map((brand, index) => (
+    <option key={index} value={brand}>
+      {brand}
+    </option>
+  ))}
+</select>
+
 
   // Generar un array con los años desde 1999 hasta el año actual
   const years = [];
@@ -96,107 +120,118 @@ const VehicleForm = () => {
   }
 
   // Generar un array con colores disponibles
-  const colors = ["Selecciona el Color", "Blanco", "Negro", "Rojo", "Azul", "Gris", "Plata", "Amarillo", "Multicolor"];
+  const colors = [
+    "Selecciona el Color",
+    "Blanco", "Negro", "Gris", "Plata", "Rojo", "Azul", "Amarillo",
+    "Verde", "Naranja", "Marrón", "Beige", "Blanco Perla", "Negro Mate",
+    "Gris Oscuro", "Azul Marino", "Rojo Carmesí", "Amarillo Brillante","Multicolor",
+  ];
+  
 
   return (
-    <div className="max-w-md mx-auto">
-      <h2 className="text-2xl text-center font-bold mb-4">Crear Vehículo Mandaderos</h2>
-      <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">Type:</label>
-          <select
-            name="type_vehicle"
-            value={formData.type_vehicle}
-            onChange={handleInputChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          >
-            <option value="">Select Type</option>
-            <option value="car">Carro</option>
-            <option value="bike">Moto</option>
-            <option value="bicycle">Bicicleta</option>
-          </select>
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">Brand:</label>
-          <select
-            name="brand_vehicle"
-            value={formData.brand_vehicle}
-            onChange={handleInputChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          >
-            <option value="">Seleciona la Marca</option>
-            {vehicleBrands.map((brand, index) => (
-              <option key={index} value={brand}>
-                {brand}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">Plate:</label>
-          <input
-            type="text"
-            name="plate_vehicle"
-            value={formData.plate_vehicle}
-            onChange={handleInputChange}
-            className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${plateError ? 'border-red-500' : ''}`}
-            placeholder=" placa"
-          />
-          {plateError && <p className="text-red-500 text-xs italic">{plateError}</p>}
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">Model:</label>
-          <select
-            name="model_vehicle"
-            value={formData.model_vehicle}
-            onChange={handleInputChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          >
-            <option value="">Modelo (año)</option>
-            {years.map((year) => (
-              <option key={year} value={year}>
-                {year}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">Color:</label>
-          {/* Campo de entrada con la lista de colores */}
-          <select
-            name="color_vehicle"
-            value={formData.color_vehicle}
-            onChange={handleInputChange}
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          >
-            {colors.map((color, index) => (
-              <option key={index} value={color}>
-                {color}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2">Imagen:</label>
-          <input
-            type="file"
-            name="image_vehicle"
-            onChange={handleFileChange}
-            className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          />
-        </div>
-       
-        <div className="flex items-center justify-between">
-          <button
-            type="submit"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-            disabled={loading || plateError}
-          >
-            {loading ? 'Submitting...' : 'Submit'}
-          </button>
-        </div>
-      </form>
+    <div className="bg-slate-500 min-h-screen flex justify-center items-center">
+    <div className="max-w-md w-full mx-auto p-4">
+      <div className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <h1 className="text-2xl text-center font-bold mb-4">Form Vehicles</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">Type:</label>
+            <select
+  name="type_vehicle"
+  value={formData.type_vehicle}
+  onChange={handleInputChange}
+  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline cursor-pointer"
+>
+  <option value="">Select Type</option>
+  <option value="car" className="hover:bg-gray-100 cursor-pointer">Carro</option>
+  <option value="bike" className="hover:bg-gray-100 cursor-pointer">Moto</option>
+  <option value="bicycle" className="hover:bg-gray-100 cursor-pointer">Bicicleta</option>
+</select>
+
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">Brand:</label>
+            <select
+              name="brand_vehicle"
+              value={formData.brand_vehicle || ""}
+              onChange={handleInputChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline cursor-pointer"
+            >
+              <option value="">Seleciona la Marca</option>
+              {vehicleBrandsToShow.map((brand, index) => (
+                <option key={index} value={brand}>
+                  {brand}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">Plate:</label>
+            <input
+              type="text"
+              name="plate_vehicle"
+              value={formData.plate_vehicle}
+              onChange={handleInputChange}
+              className={`shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline ${plateError ? 'border-red-500' : ''}`}
+              placeholder=" placa"
+            />
+            {plateError && <p className="text-red-500 text-xs italic">{plateError}</p>}
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">Model:</label>
+            <select
+              name="model_vehicle"
+              value={formData.model_vehicle}
+              onChange={handleInputChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline cursor-pointer"
+            >
+              <option value="">Modelo (año)</option>
+              {years.map((year) => (
+                <option key={year} value={year}>
+                  {year}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">Color:</label>
+            {/* Campo de entrada con la lista de colores */}
+            <select
+              name="color_vehicle"
+              value={formData.color_vehicle}
+              onChange={handleInputChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline cursor-pointer"
+            >
+              {colors.map((color, index) => (
+                <option key={index} value={color}>
+                  {color}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">Imagen:</label>
+            <input
+              type="file"
+              name="image_vehicle"
+              onChange={handleFileChange}
+              className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline cursor-pointer"
+            />
+          </div>
+         
+          <div className="flex items-center justify-between">
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              disabled={loading || plateError}
+            >
+              {loading ? 'Submitting...' : 'Submit'}
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
+    /</div>
   );
 };
 
