f12289329d1dab60c9ddd4bd5abbf0ba28e0b13f
cristianUrbano <criurbano316@hotmail.com>
2024-03-22 21:23:09 -0500

    actulizacion modulo manders v2

--- src/Components/Manders/MandersList.jsx
+++ src/Components/Manders/MandersList.jsx
@@ -90,68 +90,65 @@ function MandersList() {
     <div className="container mx-auto px-4">
       <h2 className="text-2xl font-bold mb-4">Mander List</h2>
       {alertMessage && <div className="text-red-500">{alertMessage}</div>}
-      {showUpdateForm ? (
-        <UpdateManderForm manderId={selectedMander} onUpdate={handleUpdate} onClose={() => setShowUpdateForm(false)} /> // Cambiar a manderId={selectedMander}
-  
+      {showVehicleForm ? (
+        <VehicleForm onCreate={handleCreateVehicle} onClose={() => setShowVehicleForm(false)} />
+      ) : showUpdateForm ? (
+        <UpdateManderForm manderId={selectedMander} onUpdate={handleUpdate} onClose={() => setShowUpdateForm(false)} />
+      ) : showCreateForm ? (
+        <ManderForm onCreate={handleCreate} onClose={() => setShowCreateForm(false)} />
       ) : (
         <>
-          {showCreateForm ? (
-            <ManderForm onCreate={handleCreate} onClose={() => setShowCreateForm(false)} />
-          ) : (
-            <>
-              <div className="flex mb-4">
-                <input
-                  type="text"
-                  className="flex-1 border-2 border-gray-300 bg-white h-10 px-5 rounded-lg text-sm focus:outline-none"
-                  placeholder="Search..."
-                  onChange={handleSearchMander}
+          <div className="flex mb-4">
+            <input
+              type="text"
+              className="flex-1 border-2 border-gray-300 bg-white h-10 px-5 rounded-lg text-sm focus:outline-none"
+              placeholder="Search..."
+              onChange={handleSearchMander}
+            />
+            <button
+              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded ml-2"
+              onClick={handleCreateMander}
+            >
+              New Mander
+            </button>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+            {filteredManders.map((mander, index) => (
+              <div key={index} className="bg-white rounded-lg shadow-md p-4">
+                <img
+                  src={mander.image_mander}
+                  alt={`Image of ${mander.user.name_user} ${mander.user.lastname_user}`}
+                  className="w-full h-auto mb-2 rounded-lg"
                 />
+                <p className="text-sm mb-1"><span className="font-bold">User:</span> {mander.user.name_user} {mander.user.lastname_user}</p>
+                <p className="text-sm mb-1"><span className="font-bold">Email:</span> {mander.user.email_account}</p>
+                <p className="text-sm mb-1"><span className="font-bold">Phone:</span> {mander.user.phone_user}</p>
+                <p className="text-sm mb-1"><span className="font-bold">Has Car:</span> {mander.ishavecar_mander ? 'Yes' : 'No'}</p>
+                <p className="text-sm mb-1"><span className="font-bold">Has Motorcycle:</span> {mander.ishavemoto_mander ? 'Yes' : 'No'}</p>
+                <p className="text-sm mb-1"><span className="font-bold">Is Active:</span> {mander.isactive_mander ? 'Yes' : 'No'}</p>
+                <p className="text-sm mb-1"><span className="font-bold">Is Validated:</span> {mander.isvalidate_mander ? 'Yes' : 'No'}</p>
+                <p className="text-sm mb-1"><span className="font-bold">Address:</span> {mander.address_mander}</p>
+                <p className="text-sm mb-1"><span className="font-bold">CC:</span> {mander.cc_mander}</p>
                 <button
-                  className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded ml-2"
-                  onClick={handleCreateMander}
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded mr-2"
+                  onClick={() => handleEditMander(mander.id_mander)}
                 >
-                  New Mander
+                  Edit
+                </button>
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
+                  onClick={() => handleCreateVehicle()}
+                >
+                  Vehicle
                 </button>
               </div>
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-                {filteredManders.map((mander, index) => (
-                  <div key={index} className="bg-white rounded-lg shadow-md p-4">
-                    <img
-                       src={mander.image_mander}
-                        alt={`Image of ${mander.user.name_user} ${mander.user.lastname_user}`}
-                        className="w-full h-auto mb-2 rounded-lg"
-                   />
-                    <p className="text-sm mb-1"><span className="font-bold">User:</span> {mander.user.name_user} {mander.user.lastname_user}</p>
-                    <p className="text-sm mb-1"><span className="font-bold">Email:</span> {mander.user.email_account}</p>
-                    <p className="text-sm mb-1"><span className="font-bold">Phone:</span> {mander.user.phone_user}</p>
-                    <p className="text-sm mb-1"><span className="font-bold">Has Car:</span> {mander.ishavecar_mander ? 'Yes' : 'No'}</p>
-                    <p className="text-sm mb-1"><span className="font-bold">Has Motorcycle:</span> {mander.ishavemoto_mander ? 'Yes' : 'No'}</p>
-                    <p className="text-sm mb-1"><span className="font-bold">Is Active:</span> {mander.isactive_mander ? 'Yes' : 'No'}</p>
-                    <p className="text-sm mb-1"><span className="font-bold">Is Validated:</span> {mander.isvalidate_mander ? 'Yes' : 'No'}</p>
-                    <p className="text-sm mb-1"><span className="font-bold">Address:</span> {mander.address_mander}</p>
-                    <p className="text-sm mb-1"><span className="font-bold">CC:</span> {mander.cc_mander}</p>
-                    <button
-                      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded mr-2"
-                      onClick={() => handleEditMander(mander.id_mander)}
-                    >
-                      Edit
-                    </button>
-                    <button
-                      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
-                      onClick={() => handleCreateVehicle()}
-                    >
-                      Vehicle
-                    </button>
-                  </div>
-                ))}
-              </div>
-              {showVehicleForm && <VehicleForm onClose={() => setShowVehicleForm(false)} />}
-            </>
-          )}
+            ))}
+          </div>
         </>
       )}
     </div>
   );
+  
   
 }
 
