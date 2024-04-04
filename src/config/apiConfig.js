3ce56366815fc31d433fe81f56e81c440b3ccbc1
ccristianss <cristian.silva9402@gmail.com>
2024-04-04 18:10:27 -0500

    Se crear dockercompose

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,3 +1,6 @@
+//Para Uso con Docker
+//const apiUrl = 'http://localhost:8000/';
+//Para uso con backend desplegado en Azure
 const apiUrl = 'https://manders.azurewebsites.net';
 
 export default apiUrl;
