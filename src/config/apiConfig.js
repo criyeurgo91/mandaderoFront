7a17f09e43f2b7f7dfe2d2c7ad33b81303f1f304
ccristianss <cristian.silva9402@gmail.com>
2024-04-05 00:06:13 -0500

    Cambio

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 //Para Uso con Docker
-//const apiUrl = 'http://localhost:8000/';
+const apiUrl = 'http://localhost:8000/';
 //Para uso con backend desplegado en Azure
-const apiUrl = 'https://manders.azurewebsites.net';
+//const apiUrl = 'https://manders.azurewebsites.net';
 
 export default apiUrl;
