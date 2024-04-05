8a94f5023a462073a909d3fa4d208ec857162b7a
ccristianss <cristian.silva9402@gmail.com>
2024-04-05 05:08:03 -0500

    apiUrl

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,7 +1,7 @@
 //Para Uso con Docker
 var protocol = window.location.protocol;
 var host = window.location.host;
-var apiUrl = protocol + '//' + host + '8000';
+var apiUrl = protocol + '//' + host + ':8000';
 //const apiUrl = 'http://localhost:8000';
 //Para uso con backend desplegado en Azure
 //const apiUrl = 'https://manders.azurewebsites.net';
