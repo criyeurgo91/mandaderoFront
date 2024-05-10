31ba111de8540095e42d58f6e0b7fff972a91f13
quimbaya <john.quimbaya1@misena.edu.co>
2024-05-10 18:15:50 -0500

    cargo actualizacion y requst

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -6,7 +6,8 @@ var host = window.location.host;
 //Para uso con backend desplegado en Azure
 //const apiUrl = 'https://manders.azurewebsites.net';
 //Para uso con backend desplegado en Azure2
-const apiUrl = 'https://mandaderos.azurewebsites.net';
+//const apiUrl = 'https://mandaderos.azurewebsites.net';
+const apiUrl = 'https://mandaderos3.azurewebsites.net';
 //para uso local
 //const apiUrl = 'http://127.0.0.1:8000'
 
