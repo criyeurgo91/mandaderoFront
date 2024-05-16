c7bf88363395bb227026a6f52c854d62ece75c52
cristianUrbano <criurbano316@hotmail.com>
2024-05-15 20:36:57 -0500

    modificacion estetica login y se agrega splash

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -2,15 +2,20 @@ import React, { useState } from 'react';
 import axiosInstance from '../../Logic/AxiosInstance';
 import { useNavigate } from 'react-router-dom';
 import apiUrl from '../../config/apiConfig';
+import logo from '../../assets/logo.png'
+import Wavify from 'react-wavify'
+import SplashScreen from '../Splash/SplashScreen';
 
 const LoginForm = ({ onLogin }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
   
     try {
       const response = await axiosInstance.post(`${apiUrl}/api2/login2/`, {
@@ -33,42 +38,66 @@ const LoginForm = ({ onLogin }) => {
       setError('Error al iniciar sesión. Por favor, verifica tu correo electrónico y contraseña.');
       console.error('Error al iniciar sesión:', error);
     }
+    finally {
+      setLoading(false);
+    }
   };
+
+  if (loading) {
+    return <SplashScreen />;
+  }
   
 
   return (
-    <div className="flex justify-center items-center h-screen bg-slate-900">
-      <div className="bg-white rounded-lg p-8 shadow-md w-80">
-        <h2 className="text-2xl font-bold mb-6 text-center">Administrador</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label htmlFor="email" className="block text-sm font-semibold mb-2">Correo:</label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="password" className="block text-sm font-semibold mb-2">Contraseña:</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-              required
-            />
-          </div>
-          <button type="submit" className="w-full bg-blue-500 text-white rounded-md py-2">Ingresar</button>
-        </form>
-        {error && <p className="text-red-500 mt-4 text-center">{error}</p>}
+    <div className="relative">
+      <div className="flex flex-col justify-center items-center h-screen bg-blue-500 overflow-hidden">
+        <div className="text-center mb-4 z-10">
+          <img src={logo} alt="Logo" className="mx-auto mb-2 w-32 h-auto" />
+        </div>
+        <div className="bg-white rounded-lg p-8 shadow-md w-80 z-10">
+          <h2 className="text-2xl font-bold mb-6 text-center">Inicio de Sesion</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <label htmlFor="email" className="block text-sm font-semibold mb-2">Correo:</label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+                required
+              />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="password" className="block text-sm font-semibold mb-2">Contraseña:</label>
+              <input
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+                required
+              />
+            </div>
+            <button type="submit" className="w-full bg-blue-500 text-white rounded-md py-2">Ingresar</button>
+          </form>
+          {error && <p className="text-red-500 mt-4 text-center">{error}</p>}
+        </div>
+        <Wavify
+          fill="#1e3a8a"
+          paused={false}
+          options={{
+            height: 20,
+            amplitude: 40,
+            speed: 0.15,
+            points: 3
+          }}
+          className="absolute bottom-0 w-full"
+        />
       </div>
     </div>
   );
 };
+
 
 export default LoginForm;
---
+++ src/Components/Splash/SplashScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SplashScreen = () => {
+  return (
+    <div className="flex justify-center items-center h-screen bg-blue-500">
+      <h1 className="text-white text-2xl">Cargando...</h1>
+    </div>
+  );
+};
+
+export default SplashScreen;
