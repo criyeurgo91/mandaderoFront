7c033077d0cfaf1276c2baa0719d4f1cc99ac66b
cristianUrbano <criurbano316@hotmail.com>
2024-05-26 13:43:49 -0500

    unificacion p1

--- src/Components/Request/RequestFilter.jsx
+++ src/Components/Request/RequestFilter.jsx
@@ -3,23 +3,25 @@ import { Link } from 'react-router-dom';
 
 const RequestFilter = ({ handleSearch, handleStatusFilter }) => {
   return (
-    <div className="flex justify-start mb-4">
-      <input
-        type="text"
-        placeholder="Buscar..."
-        className="w-1/2 border-2 border-blue-500 bg-sky-950 h-10 px-5 rounded-lg text-sm text-white focus:outline-none mr-4"
-        onChange={handleSearch}
-      />
-      <select
-        className="w-1/6 border-2 border-blue-500 bg-sky-950 h-10 px-5 rounded-lg text-sm text-white focus:outline-none"
-        onChange={handleStatusFilter}
-      >
-        <option value="pendiente">Pendiente</option>
-        <option value="proceso">Proceso</option>
-        <option value="finalizado">Finalizado</option>
-      </select>
+    <div className="flex justify-between items-center mb-4">
       <div>
-      <Link to="request/ubicacion-manders" className="bg-sky-800 text-white py-2 px-4 rounded-md">Ubicación Mandaderos</Link>
+        <input
+          type="text"
+          placeholder="Buscar..."
+          onChange={handleSearch}
+          className="border-2 border-blue-500 px-4 py-2 rounded-lg mr-4"
+        />
+        <select
+          onChange={handleStatusFilter}
+          className="border-2 border-blue-500 px-4 py-2 rounded-lg"
+        >
+          <option value="pendiente">Pendientes</option>
+          <option value="proceso">En proceso</option>
+          <option value="finalizado">Finalizados</option>
+        </select>
+      </div>
+      <div>
+      <Link to="request/ubicacion-manders" className="bg-sky-800 text-white py-2 px-4 rounded-md">Ubicación Manders</Link>
 
       </div>
     </div>
