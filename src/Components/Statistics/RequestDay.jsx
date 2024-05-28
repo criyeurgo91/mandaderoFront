f7a3dc74be12b9e91d8d8de6b1714dfb48ab49ed
junigomez <amaya261094gomez@gmail.com>
2024-05-27 20:26:34 -0500

    se actualizo RequestDay

--- src/Components/Statistics/RequestDay.jsx
+++ src/Components/Statistics/RequestDay.jsx
@@ -1,31 +1,15 @@
 
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Bar, BarChart, CartesianGrid, Legend, ResponsiveContainer, Tooltip, LabelList } from 'recharts';
-import apiUrl from '../../config/apiConfig';
 
-
-const RequestDay = ({data}) => {
+const RequestDay = ({ data }) => {
   // Verificar si data.requests está definido antes de acceder a value_by_day
   const valueByDay = data.requests?.value_by_day || [];
 
-  const servicesPerDay = {};
 
-  // Procesar los datos recibidos de props
-  valueByDay.forEach(request => {
-    const formattedDate = request.day.split('T')[0]; // Obtener solo la fecha (yyyy-MM-dd)
-    console.log(formattedDate)
-
-    if (servicesPerDay[formattedDate]) {
-      servicesPerDay[formattedDate]++;
-    } else {
-      servicesPerDay[formattedDate] = 1;
-    }
-  });
-
-  // Convertir los datos procesados en un formato compatible con Recharts
-  const processedData = Object.keys(servicesPerDay).map(date => ({
-    date: date,
-    recibidas: servicesPerDay[date],
+  const processedData = valueByDay.map(request => ({
+    date: request.day.split('T')[0], // Obtener solo la fecha (yyyy-MM-dd)
+    recibidas: request.count,
   }));
 
   return (
@@ -34,11 +18,11 @@ const RequestDay = ({data}) => {
       <ResponsiveContainer width="100%" height="85%">
         <BarChart
           data={processedData}
-          margin={{ top:20, right: 0, left: 0, bottom: 20 }}
+          margin={{ top: 20, right: 0, left: 0, bottom: 20 }}
         >
           <CartesianGrid strokeDasharray="4 1 2" />
           <Bar dataKey="recibidas" fill="#6b48ff" barSize={30}>
-            <LabelList dataKey="date" position="top"/>
+            <LabelList dataKey="date" position="top" />
           </Bar>
           <Tooltip cursor={{ fill: 'rgba(0,0,0,0.1)' }} formatter={(value) => `${value} solicitudes`} />
           <Legend />
@@ -49,6 +33,8 @@ const RequestDay = ({data}) => {
 };
 
 export default RequestDay;
+
+
 
 
 
