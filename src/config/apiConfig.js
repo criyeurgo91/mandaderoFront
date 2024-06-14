6d969190d22030f3443a69b0be43a4de6aac8ce4
cristianUrbano <criurbano316@hotmail.com>
2024-06-13 20:38:24 -0500

    actualizacion 130624

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -4,11 +4,11 @@ var host = window.location.host;
 // var apiUrl = protocol + '//' + host + ':8000';
 //// const apiUrl = 'http://localhost:8000';
 //Para uso con backend desplegado en Azure
-const apiUrl = 'https://manders.azurewebsites.net';
+//const apiUrl = 'https://manders.azurewebsites.net';
 //Para uso con backend desplegado en Azure2
 //const apiUrl = 'https://mandaderos.azurewebsites.net';
 //Para uso con backend desplegado en Azure3
-//const apiUrl = 'https://mandaderos3.azurewebsites.net';
+const apiUrl = 'https://mandaderos3.azurewebsites.net';
 //para uso local
 //const apiUrl = 'http://127.0.0.1:8000'
 
